cab3c52e77db05e9aa3187d7193aa792eaed9e62
Nathan Yang <nathan.yang@usask.ca>
2018-04-17 15:50:59 -0600

    modified sampling.sh

--- sampling.sh
+++ sampling.sh
@@ -8,7 +8,7 @@ for folder in ref_?_tgt_?_$chrom; do
 		( bcftools view -Oz -o ${prefix}_ref.fmlt5.vcf.gz \
 				-S ${prefix}_ref_samples.txt \
 				-i 'f_missing<0.05' $originalVCF
-		tabix -p vcf ${prefix}_ref.vcf.gz
+		tabix -p vcf ${prefix}_ref.fmlt5.vcf.gz
 		bcftools view -Oz -o ${prefix}_tgt.vcf.gz \
 				-S ${prefix}_tgt_samples.txt \
 				-R ${prefix}_ref.fmlt5.vcf.gz $originalVCF
