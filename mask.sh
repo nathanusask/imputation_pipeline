1fa8c4373ffe4db8ddcdd35b93916b0e97932055
Nathan Yang <nathan.yang@usask.ca>
2018-04-30 10:42:15 -0600

    removed several lines from the old version

--- mask.sh
+++ mask.sh
@@ -4,17 +4,14 @@
 # chromosome number should be in format 'chr#', e.g., chr1
 
 chrom=$1
-for folder in ref_?_tgt_?_$chrom; do
-	for i in {1..10}; do
+for i in {1..10}; do
+	for folder in ref_?_tgt_?_$chrom; do
 		( prefix=${folder}/rep_$i
 		tgt_file=${prefix}_tgt.vcf.gz
 		tmp=${tgt_file##*/}
 		./mask.py --filename $tgt_file --prefix ${tmp/.vcf.gz/}.mask --directory $folder
-		vcf_masked=${tmp/.vcf.gz/}.mask.vcf
-		bgzip -f ${tmp/.vcf.gz/}.mask.vcf
-		tabix -p vcf ${tmp/.vcf.gz/}.mask.vcf.gz
-		gzip ${tmp/.vcf.gz/}.mask.txt ) &
+		) &
 	done
+	wait
 done
-
 wait
